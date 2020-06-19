468d189cb091779765ec7653fa53c8531992495c
Karl Johan Heimark <kjh@wgtwo.com>
2020-06-19 12:12:35 +0200

    Add unary_error function
    In `grpcbox_stream` there is no way to send a grpc error if the
    `grpcbox_stream` isn't the ownign process. This is the case when you
    would typically return a success with `grpcbox_stream:unary_reply`.
    
    This change adds the `grpcbox_stream:unary_error` function for this case

--- src/grpcbox_stream.erl
+++ src/grpcbox_stream.erl
@@ -108,10 +108,16 @@ on_receive_headers(Headers, State=#state{ctx=_Ctx}) ->
               infinity ->
                   grpcbox_metadata:new_incoming_ctx(Metadata);
               D ->
+<<<<<<< HEAD
                   Deadline = max(0, erlang:convert_time_unit(D, nanosecond, millisecond)),
                   erlang:start_timer(Deadline, self(), <<"grpc-timeout">>),
                   ctx:with_deadline_after(grpcbox_metadata:new_incoming_ctx(Metadata), D, nanosecond)
           end,
+=======
+                  ctx:with_deadline_after(grpcbox_metadata:new_incoming_ctx(Metadata), D, nanosecond)
+          end,
+
+>>>>>>> 681a23a (Add unary_error function)
     FullPath = proplists:get_value(<<":path">>, Headers),
     %% wait to rpc_begin here since we need to know the method
     Ctx1 = ctx:with_value(Ctx, grpc_server_method, FullPath),
